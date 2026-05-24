e1ea0b5b24c524c59f7fb45a4e597687103ab7cd
ARINA KIM <arinakim@MacBook-Air-ARINA.local>
2026-05-24 10:22:58 +0900

    swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,41 @@
-import './script.js'
-document.addEventListener("DOMContentLoaded", function () {
-  const btn = document.querySelector(".hide");
-  const brands = document.querySelector(".brands");
-  const text = btn.querySelector("p");
-  let swiper = null;
+import Swiper from 'swiper'
+import { Pagination } from 'swiper/modules'
+// import Swiper and modules styles
+// import 'swiper/css'
+// import 'swiper/css/pagination'
+
+document.addEventListener('DOMContentLoaded', function () {
+  const btn = document.querySelector('.hide')
+  const brands = document.querySelector('.brands')
+  const text = btn.querySelector('p')
+  let swiper = null
 
   function initSwiper() {
     if (window.innerWidth < 768 && swiper === null) {
-      swiper = new Swiper(".mySwiper", {
+      swiper = new Swiper('.mySwiper', {
         slidesPerView: 1.2,
         spaceBetween: 10,
+        // modules: [Pagination],
         pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      });
+          el: '.swiper-pagination',
+          clickable: true
+        }
+      })
     }
 
     if (window.innerWidth >= 768 && swiper !== null) {
-      swiper.destroy(true, true);
-      swiper = null;
+      swiper.destroy(true, true)
+      swiper = null
     }
   }
 
-  btn.addEventListener("click", () => {
-    brands.classList.toggle("show-midle");
-    text.textContent = brands.classList.contains("show-midle")
-      ? "Скрыть"
-      : "Показать все";
-  });
+  btn.addEventListener('click', () => {
+    brands.classList.toggle('show-midle')
+    text.textContent = brands.classList.contains('show-midle')
+      ? 'Скрыть'
+      : 'Показать все'
+  })
 
   // initSwiper();
-  window.addEventListener("resize", initSwiper);
-});
+  window.addEventListener('resize', initSwiper)
+})
